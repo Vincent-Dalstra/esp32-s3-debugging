6e2fa9db56f5ff2c741714e09044caa468ca104d
Vincent Dalstra <vincent.dalstra@outlook.com>
2023-09-15 15:39:24 +0800

    Discovered interesting bug while testing PCB
    Implemented a boards file for the custom PCB's that use the ESP32-S3
    with 4MB/2MB of Flash/PSRAM, and therefore require different settings
    to the devkitC. I copied most of them from the Adafruit feather, except
    the PSRAM compile flag (because it would immediately crash).
    
    However, I discovered some weird behaviour, which also happens on the
    devkitC as it turns out. Any time spent 'paused' using the JTAG
    debugger e.g. at a breakpoint or stepping through, is counted against
    the next call to delay()! It makes no sense, but unfortunately the
    functions are written in assembly, making it hard to figure out...

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "esp32-hal.h"
 
 
+// Only Dev boards have the Built-in LED.
+#define SIMPLE true
+
+#if SIMPLE == false
 void setup()
 {
   Serial.begin(115200);
@@ -26,3 +30,49 @@ void loop()
   delay(500);
 }
 
+#elif SIMPLE == true
+
+bool trueDelay(int millis);
+
+TickType_t xLastWakeTime;
+BaseType_t xWasDelayed;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(15, OUTPUT);    // One of the relays on the ESP32-S3-2R board, just to test with.
+  pinMode(16, OUTPUT);    // Other Relay. Both relays have indicator LED's as well.
+  xLastWakeTime = xTaskGetTickCount();
+}
+
+void loop()
+{ 
+  while (true)
+  {
+    
+    Serial.println("Hello world!");
+
+    //digitalWrite(LED_BUILTIN, LOW);
+    //delay(500);
+    digitalWrite(15, HIGH);
+    delay(500);             // Uses vTaskDelay()
+    digitalWrite(15, LOW);
+    vTaskDelay(500);
+    digitalWrite(16, HIGH);
+    trueDelay(5000);
+    digitalWrite(16, LOW);
+    trueDelay(5000);
+  }
+}
+
+
+bool trueDelay(int millis)
+{
+  TickType_t start = xTaskGetTickCount();
+  TickType_t ticks = (millis / portTICK_PERIOD_MS);
+  return xTaskDelayUntil( &start, ticks );          // Suffers the same issue as vTaskDelay();
+}
+
+
+#endif
